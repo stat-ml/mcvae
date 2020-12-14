Bug for IWAE in nll computation

models/samplers.py
@@ -122,9 +122,9 @@ class HMC(nn.Module):
         return z_, p_
 
     def get_grad(self, z, target, x=None):
-        z = z.detach().requires_grad_(True)
+        z_ = z.detach().requires_grad_(True)
         with torch.enable_grad():
-            grad = _get_grad(z=z, target=target, x=x)
+            grad = _get_grad(z=z_, target=target, x=x)
             return grad
 
 
@@ -190,9 +190,9 @@ class MALA(nn.Module):
         return z_new, a.to(torch.float32), current_log_alphas
 
     def get_grad(self, z, target, x=None):
-        z = z.detach().requires_grad_(True)
+        z_ = z.detach().requires_grad_(True)
         with torch.enable_grad():
-            grad = _get_grad(z=z, target=target, x=x)
+            grad = _get_grad(z=z_, target=target, x=x)
             return grad
 
 
@@ -277,7 +277,7 @@ class ULA(nn.Module):
         return z_new, proposal_density_numerator - proposal_density_denominator + self.log_jac, a
 
     def get_grad(self, z, target, x=None):
-        z = z.detach().requires_grad_(True)
+        z_ = z.detach().requires_grad_(True)
         with torch.enable_grad():
-            grad = _get_grad(z=z, target=target, x=x)
+            grad = _get_grad(z=z_, target=target, x=x)
             return grad

models/vaes.py
@@ -159,8 +159,7 @@ class Base(pl.LightningModule):
                                         annealing_logdens=annealing_logdens(beta=beta[i]), nll=True)[0]
 
                 sum_log_weights += (beta[i + 1] - beta[i]) * (self.joint_logdensity()(z=z, x=x) - init_logdens(z=z))
-
-            sum_log_weights = sum_log_weights.view(x.shape[0], self.num_samples)
+            sum_log_weights = sum_log_weights.view(batch[0].shape[0], n_samples)
             batch_nll_estimator = torch.logsumexp(sum_log_weights,
                                                   dim=-1)  ###Should be a vector of batchsize containing nll estimator for each term of the batch
 