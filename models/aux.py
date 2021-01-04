Changes: 1) Added skip connections to encoder 2) Added gradient update skipping and gradient clipping 3) Omitted lr scheduler 4) Minor typos fixed

models/aux.py
@@ -2,9 +2,17 @@ import torch
 import torch.nn as nn
 
 
+def pad_channels(t, width):
+    d1, d2, d3, d4 = t.shape
+    empty = torch.zeros(d1, width, d3, d4, device=t.device)
+    empty[:, :d2, :, :] = t
+    return empty
+
+
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels, act_func, mid_channels=None):
+    def __init__(self, in_channels, out_channels, act_func, mid_channels=None, skip_connection=True):
         super().__init__()
+        self.skip_connection = skip_connection
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
@@ -15,7 +23,11 @@ class DoubleConv(nn.Module):
         )
 
     def forward(self, x):
-        return self.double_conv(x)
+        conv = self.double_conv(x)
+        if self.skip_connection and x.shape[1] <= conv.shape[1]:
+            return pad_channels(x, conv.shape[1]) + conv
+        else:
+            return conv
 
 
 class Down(nn.Module):
@@ -33,15 +45,15 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, act_func, bilinear=True, size=None):
+    def __init__(self, in_channels, out_channels, act_func, upsampling=True, size=None):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
+        # if upsampling, use the normal convolutions to reduce the number of channels
+        if upsampling:
             if size is None:
-                self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+                self.up = nn.Upsample(scale_factor=2, mode='nearest')  # , align_corners=True
             else:
-                self.up = nn.Upsample(size=size, mode='bilinear', align_corners=True)
+                self.up = nn.Upsample(size=size, mode='nearest')  # align_corners=True
             self.conv = DoubleConv(in_channels, out_channels, act_func, in_channels // 2)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)

models/decoders.py
@@ -36,19 +36,19 @@ class FC_decoder_mnist(nn.Module):
 
 
 class CONV_decoder(nn.Module):
-    def __init__(self, act_func, hidden_dim, n_channels, shape, bilinear=True):
+    def __init__(self, act_func, hidden_dim, n_channels, shape, upsampling=True):
         super().__init__()
-        self.bilinear = bilinear
-        factor = 2 if bilinear else 1
+        self.upsampling = upsampling
+        factor = 2 if upsampling else 1
         num_maps = 16
         shape_init = shape
         shape = shape // 8
         self.net = nn.Sequential(
             nn.Linear(hidden_dim, (8 * num_maps // factor) * (shape ** 2)),
             View(((8 * num_maps // factor), shape, shape)),
-            Up(8 * num_maps // factor, 4 * num_maps // factor, act_func, bilinear, (shape * 2, shape * 2)),
-            Up(4 * num_maps // factor, 2 * num_maps // factor, act_func, bilinear, (shape * 4, shape * 4)),
-            Up(2 * num_maps // factor, num_maps, act_func, bilinear, (shape_init, shape_init)),
+            Up(8 * num_maps // factor, 4 * num_maps // factor, act_func, upsampling, (shape * 2, shape * 2)),
+            Up(4 * num_maps // factor, 2 * num_maps // factor, act_func, upsampling, (shape * 4, shape * 4)),
+            Up(2 * num_maps // factor, num_maps, act_func, upsampling, (shape_init, shape_init)),
             OutConv(num_maps, n_channels)
         )
         self.hidden_dim = hidden_dim

models/encoders.py
@@ -19,11 +19,11 @@ class FC_encoder_mnist(nn.Module):
 
 
 class CONV_encoder(nn.Module):
-    def __init__(self, act_func, hidden_dim, n_channels, shape, bilinear=True):
+    def __init__(self, act_func, hidden_dim, n_channels, shape, upsampling=True):
         super().__init__()
         self.n_channels = n_channels
-        self.bilinear = bilinear
-        factor = 2 if bilinear else 1
+        self.upsampling = upsampling
+        factor = 2 if upsampling else 1
         num_maps = 16
         num_units = ((shape // 8) ** 2) * (8 * num_maps // factor)
 

models/samplers.py
@@ -164,7 +164,7 @@ class MALA(nn.Module):
         update = torch.sqrt(2 * self.step_size) * eps + self.step_size * self.get_grad(z=z_old,
                                                                                        target=target,
                                                                                        x=x)
-        return z_old + update, eps
+        return z_old + update, update, eps
 
     def make_transition(self, z, target, x=None):
         """
@@ -180,12 +180,12 @@ class MALA(nn.Module):
         ############ Then we compute new points and densities ############
         std_normal = torch.distributions.Normal(loc=self.zero, scale=self.one)
 
-        z_upd, eps = self._forward_step(z_old=z, x=x, target=target)
+        z_upd, update, eps = self._forward_step(z_old=z, x=x, target=target)
 
         target_log_density_upd = target(z=z_upd, x=x)
         target_log_density_old = target(z=z, x=x)
 
-        eps_reverse = (z - z_upd - self.step_size * self.get_grad(z=z_upd, target=target, x=x)) / torch.sqrt(
+        eps_reverse = (-update - self.step_size * self.get_grad(z=z_upd, target=target, x=x)) / torch.sqrt(
             2 * self.step_size)
         proposal_density_numerator = std_normal.log_prob(eps_reverse).sum(1)
         proposal_density_denominator = std_normal.log_prob(eps).sum(1)

models/vaes.py
@@ -249,7 +249,7 @@ class IWAE(Base):
 
 
 class BaseAIS(Base):
-    def __init__(self, step_size, K, beta=None, **kwargs):
+    def __init__(self, step_size, K, beta=None, grad_skip_val=0., grad_clip_val=0., **kwargs):
         super().__init__(**kwargs)
         self.save_hyperparameters()
         self.K = K
@@ -262,6 +262,8 @@ class BaseAIS(Base):
         if beta is None:
             beta = torch.tensor(np.linspace(0., 1., self.K + 2), dtype=torch.float32)
         self.register_buffer('beta', beta)
+        self.grad_skip_val = grad_skip_val
+        self.grad_clip_val = grad_clip_val
 
     def configure_optimizers(self):
         lambda_lr = lambda epoch: 10 ** (-epoch / 7.0)
@@ -271,7 +273,7 @@ class BaseAIS(Base):
         encoder_optimizer = torch.optim.Adam(list(self.encoder_net.parameters()) + list(self.transitions.parameters()),
                                              lr=1e-3, eps=1e-4)
         encoder_scheduler_lr = torch.optim.lr_scheduler.LambdaLR(encoder_optimizer, lambda_lr)
-        return [decoder_optimizer, encoder_optimizer], [decoder_scheduler_lr, encoder_scheduler_lr]
+        return [decoder_optimizer, encoder_optimizer]  # , [decoder_scheduler_lr, encoder_scheduler_lr]
 
     def run_transitions(self, z, x, mu, logvar, inference_part=False):
         init_logdens = lambda z: torch.distributions.Normal(loc=mu, scale=torch.exp(0.5 * logvar)).log_prob(
@@ -295,7 +297,7 @@ class BaseAIS(Base):
         return output
 
     def update_stepsize(self, accept_rate):
-        accept_rate = list(accept_rate.cpu().data.squeeze().numpy())
+        accept_rate = list(accept_rate.cpu().data.numpy())
         for l in range(0, self.K - 1):
             if accept_rate[l] < self.epsilon_target:
                 self.epsilons[l] *= self.epsilon_decrease_alpha
@@ -434,7 +436,6 @@ class AIS_VAE(BaseAIS):
                     self.joint_logdensity()(z=z_transformed, x=x) - init_logdens(z=z))
             all_acceptance = torch.cat([all_acceptance, directions[None]])
 
-        all_acceptance = torch.cat([all_acceptance, directions[None]])
         self.update_stepsize(all_acceptance.mean(1))
         return z_transformed, sum_log_weights, sum_log_alpha, all_acceptance
 
@@ -474,19 +475,60 @@ class AIS_VAE(BaseAIS):
 
     def training_step(self, batch, batch_idx, optimizer_idx):
         x, _ = batch
-        inference_part = {0: False, 1: True}[optimizer_idx]
-        with Stop_grads(not inference_part):
-            z, mu, logvar = self.enc_rep(x, self.num_samples)
+        if self.grad_skip_val == 0.:
+            inference_part = {0: False, 1: True}[optimizer_idx]
+            with Stop_grads(not inference_part):
+                z, mu, logvar = self.enc_rep(x, self.num_samples)
 
-        x = repeat_data(x, self.num_samples)
-        z_transformed, sum_log_weights, sum_log_alpha, all_acceptance = self.run_transitions(z=z,
-                                                                                             x=x,
-                                                                                             mu=mu,
-                                                                                             logvar=logvar,
-                                                                                             inference_part=inference_part)
-        loss = self.loss_function(sum_log_alpha=sum_log_alpha, sum_log_weights=sum_log_weights,
-                                  inference_part=inference_part)
-        return {"loss": loss}
+            x = repeat_data(x, self.num_samples)
+            z_transformed, sum_log_weights, sum_log_alpha, all_acceptance = self.run_transitions(z=z,
+                                                                                                 x=x,
+                                                                                                 mu=mu,
+                                                                                                 logvar=logvar,
+                                                                                                 inference_part=inference_part)
+            loss = self.loss_function(sum_log_alpha=sum_log_alpha, sum_log_weights=sum_log_weights,
+                                      inference_part=inference_part)
+            return {"loss": loss}
+        else:
+            (opt_decoder, opt_encoder_flows) = self.optimizers()
+
+            ## decoder
+            with Stop_grads(True):
+                z, mu, logvar = self.enc_rep(x, self.num_samples)
+
+            x_ = repeat_data(x, self.num_samples)
+            z_transformed, sum_log_weights, sum_log_alpha, all_acceptance = self.run_transitions(z=z,
+                                                                                                 x=x_,
+                                                                                                 mu=mu,
+                                                                                                 logvar=logvar,
+                                                                                                 inference_part=False)
+            loss = self.loss_function(sum_log_alpha=sum_log_alpha, sum_log_weights=sum_log_weights,
+                                      inference_part=False)
+            self.manual_backward(loss, opt_decoder)
+            grad_norm = torch.nn.utils.clip_grad_norm_(
+                self.decoder_net.parameters(), self.grad_clip_val).item()
+            if grad_norm < self.grad_skip_val:
+                opt_decoder.step()
+
+            ## encoder and flows
+            with Stop_grads(False):
+                z, mu, logvar = self.enc_rep(x, self.num_samples)
+
+            x_ = repeat_data(x, self.num_samples)
+            z_transformed, sum_log_weights, sum_log_alpha, all_acceptance = self.run_transitions(z=z,
+                                                                                                 x=x_,
+                                                                                                 mu=mu,
+                                                                                                 logvar=logvar,
+                                                                                                 inference_part=True)
+            loss = self.loss_function(sum_log_alpha=sum_log_alpha, sum_log_weights=sum_log_weights,
+                                      inference_part=True)
+            self.manual_backward(loss, opt_encoder_flows)
+            grad_norm = torch.nn.utils.clip_grad_norm_(
+                list(self.encoder_net.parameters()) + list(self.transitions.parameters()),
+                self.grad_clip_val).item()
+            if grad_norm < self.grad_skip_val:
+                opt_encoder_flows.step()
+            return
 
 
 class ULA_VAE(BaseAIS):